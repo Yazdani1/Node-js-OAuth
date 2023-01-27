0a3086f1c5d302ffbb8a82b5c8103d02657c6845
Yazdani Chowdhury <yaz4dani@gmail.com>
2023-01-27 12:45:43 +0200

    WIP

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Test")
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require('path');
+
+require("./model/db");
+
+app.use(cors());
+app.use(express.json({ limit: "4.5mb" }));
+
+// app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use("/api",require("./router/user"));
+// app.use("/api",require("./router/ExpenseBook"));
+// app.use("/api",require("./router/ExpenseCategory"));
+// app.use("/api",require("./router/ExpenseList"));
+
+
+app.listen(port, (req, res) => {
+  console.log("Server connected");
+});
