52a9542e31c2aa3748a5a9a72088ac136edfcf5a
Yazdani Chowdhury <yaz4dani@gmail.com>
2023-01-28 02:54:35 +0200

    WIP

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const CLIENT_URL = "http://localhost:3000/dashboard";
 
 router.get("/login/success", (req, res) => {
    // Generate a JWT containing the user's ID
+       // To generate the token and when user login then can store this token in the local storage
+
    const token = jwt.sign({ _id: req.user._id }, process.env.JWT_SECRET, {
     expiresIn: "24h",
   });
